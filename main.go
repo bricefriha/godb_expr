50cedb858720d17119d57245790a83696f024c45
Brice Friha <contact@bricefriha.dev>
2024-10-13 09:21:07 +0100

    cleanup - main | trimming

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"godb/services"
-	"log"
-	"os"
 )
 
 func main() {
 	fmt.Println("Engine started!!")
-	fileData, fileErr := os.ReadFile("exampleDocs/animalList.json")
-
-	if fileErr != nil {
-		log.Fatal(fileErr)
-	}
-	var res []string
-	// Read the example file
-	json.Unmarshal([]byte(string(fileData)), &res)
-
-	for i := 0; i < len(res); i++ {
-		fmt.Println(res[i])
-	}
 
 	// Add an animal
 	services.AddAnimal("cows", "exampleDocs/animalList.json")
