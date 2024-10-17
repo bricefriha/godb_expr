ea3a4f804446159341413def8d2e7c5ac46642e6
Brice Friha <contact@bricefriha.dev>
2024-10-17 16:31:11 +0100

    Service - query | extract columns

--- services/queryService.go
+++ services/queryService.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -12,11 +13,28 @@ func Execute(query string) string {
 	case "INSERT":
 		if fields[1] != "INTO" {
 			throwSyntaxError()
+			return "Wrong Syntax"
 		}
+		// get address
 		var addr []string = strings.Split(fields[2], ".")
 
 		println(addr[0])
 		println(addr[1])
+
+		re := regexp.MustCompile(`\(([^)]+)\)`)
+		columnsRaw := re.FindStringSubmatch(query)[1]
+
+		if len(columnsRaw) < 3 {
+			throwSyntaxError()
+			return "Wrong Syntax"
+		}
+
+		var columns []string = strings.Split(columnsRaw, ",")
+
+		println("columns:")
+		for i := 0; i < len(columns); i++ {
+			println(columns[i])
+		}
 
 	}
 
